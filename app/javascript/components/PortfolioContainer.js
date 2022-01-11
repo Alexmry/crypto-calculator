f51b6abc55f58306f07b3ebfee6f756509fc98e2
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-11 17:23:22 +0000

    added search and calculate components to App js file

--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -1,17 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Search from "./Search";
+import Calculate from "./Calculate";
 
 class PortfolioContainer extends Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            name: '',
-            portfolio: [],
-            search_results: [],
-            active_currency: null,
-            amount:  ''
-        }
-    }
+    this.state = {
+      name: "",
+      portfolio: [],
+      search_results: [],
+      active_currency: null,
+      amount: "",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Search />
+        <Calculate />
+      </div>
+    );
+  }
 }
 
-export default PortfolioContainer
+export default PortfolioContainer;
