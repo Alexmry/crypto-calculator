b92c277443037c450b17341c1f31c400b4858ae4
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-11 10:40:58 +0000

    updated the read me file

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,14 @@
 class Currency < ApplicationRecord
+    def request(type, url, opts = {})
+        request = HTTParty.send(type, url, opts)
+    end
+
+    def current_price
+        # url = 'https://api.coinmarketcap.com/v1/ticker/'
+        # url = 'https://api.coinbase.com/v2/prices/btc-usd/spot'
+        # url = 'https://api.coingeko.com/api/v3/coins/bitcoin'
+
+        # request = HTTParty.get(url + self.slug)
+        # response = JSON.parse(request.body)
+    end
 end
