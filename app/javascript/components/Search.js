3fee0f550b433cac2d1474e667d869f984f649b7
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-11 18:25:39 +0000

    added the react by pass csrfToken

--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -1,31 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Search extends Component {
-    constructor(props){
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return(
-            <div>
-                <h1>Cryptocurrency Portfolio Calcuator</h1>
-                <form>
-                    <div className="form-group">
-                        <label>Search for a currency:</label><br/>
-                        <input
-                            autocomplete="off"
-                            type="text"
-                            name = "name"
-                            placeholder="Add Cryptocurrency"
-                            value=""
-                            onChange={(e) => setText(e.target.value)}
-                            className="fill"
-                        />
-                    </div>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Cryptocurrency Portfolio Calcuator</h1>
+        <form>
+          <div className="form-group">
+            <label>Search for a currency:</label>
+            <br />
+            <input
+              onChange={this.props.handleChange}
+              autoComplete="off"
+              type="text"
+              name="name"
+              placeholder="Add Cryptocurrency"
+              value={this.props.name}
+              className="fill"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Search
+export default Search;
