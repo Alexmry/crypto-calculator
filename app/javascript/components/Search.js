aebca96239894f8b320a75f26995193105431fa1
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-11 17:45:17 +0000

    added search form to the search component

--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -7,7 +7,23 @@ class Search extends Component {
 
     render() {
         return(
-            <div>Search goes here</div>
+            <div>
+                <h1>Cryptocurrency Portfolio Calcuator</h1>
+                <form>
+                    <div className="form-group">
+                        <label>Search for a currency:</label><br/>
+                        <input
+                            autocomplete="off"
+                            type="text"
+                            name = "name"
+                            placeholder="Add Cryptocurrency"
+                            value=""
+                            onChange={(e) => setText(e.target.value)}
+                            className="fill"
+                        />
+                    </div>
+                </form>
+            </div>
         )
     }
 }
