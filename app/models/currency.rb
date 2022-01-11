8656082c6e206a960df410cbe14db8f405d73aa4
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-11 11:05:10 +0000

    added coincap api

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -7,8 +7,10 @@ class Currency < ApplicationRecord
         # url = 'https://api.coinmarketcap.com/v1/ticker/'
         # url = 'https://api.coinbase.com/v2/prices/btc-usd/spot'
         # url = 'https://api.coingeko.com/api/v3/coins/bitcoin'
+        # url = "https://api.coincap.io/v2/assets/bitcoin"
+        url = "https://api.coincap.io/v2/assets/"
 
-        # request = HTTParty.get(url + self.slug)
-        # response = JSON.parse(request.body)
+        request = HTTParty.get(url + self.slug)
+        response = JSON.parse(request.body)
     end
 end
