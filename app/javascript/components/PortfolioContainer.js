1d715112ff9fe8ae05b83ab8b7f3a7ff35d69362
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-11 17:08:09 +0000

    added portfolio container

---
+++ app/javascript/components/PortfolioContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class PortfolioContainer extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            name: '',
+            portfolio: [],
+            search_results: [],
+            active_currency: null,
+            amount:  ''
+        }
+    }
+}
+
+export default PortfolioContainer
